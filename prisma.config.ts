f37ac7d421c45e50029c4f2934663313de901e63
tropicans <tropicans@gmail.com>
2026-09-16 17:17:59 +0700

    fix(prisma): remove all external dependencies from prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,6 @@
-import path from "node:path";
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-    schema: path.join("prisma", "schema.prisma"),
+export default {
+    schema: "prisma/schema.prisma",
     datasource: {
         url: process.env.DATABASE_URL || "postgresql://taut_admin:taut_password_123!@localhost:5436/taut_db?schema=public",
     },
-});
+};
